9fd4057973af2a91b0b16246c9e1df51d5d9a126
王贵源 <kerbores@gmail.com>
2016-07-27 11:13:24 +0800

    fail处理之后还是把栈打印一下

--- titans/src/main/java/club/zhcs/titans/nutz/processor/KerboresFailProcessor.java
+++ titans/src/main/java/club/zhcs/titans/nutz/processor/KerboresFailProcessor.java
@@ -2,6 +2,8 @@ package club.zhcs.titans.nutz.processor;
 
 import org.nutz.json.JsonFormat;
 import org.nutz.lang.Strings;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
 import org.nutz.mvc.ActionContext;
 import org.nutz.mvc.ActionInfo;
 import org.nutz.mvc.Mvcs;
@@ -16,7 +18,7 @@ import club.zhcs.titans.utils.db.Result;
  *         create at 2015年12月9日 下午3:20:17
  */
 public class KerboresFailProcessor extends FailProcessor {
-
+	Log log = Logs.get();
 	private String okView;
 
 	@Override
@@ -28,6 +30,8 @@ public class KerboresFailProcessor extends FailProcessor {
 	@Override
 	public void process(ActionContext ac) throws Throwable {
 		if (Strings.equals(okView, "json") && ac.getError() != null) {// 产生了错误而且是json视图
+			log.debugf("response json error msg with %s ", this.getClass().getName());
+			ac.getError().printStackTrace();
 			Mvcs.write(ac.getResponse(), Result.exception(ac.getError().getMessage()), JsonFormat.nice());// 写回错误信息
 			return;
 		}
